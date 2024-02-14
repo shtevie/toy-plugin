b039fca8c49dfd1581a2baae93948c883434c96a
Steven Long <sl2263@cornell.edu>
2024-02-14 16:52:04 -0500

    add toy plugin

---
+++ src/main/java/org/example/ToyMojo.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+
+@Mojo(name = "get-developers", defaultPhase = LifecyclePhase.COMPILE)
+public class ToyMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List<String> developers = project.getDevelopers();
+        getLog().info("Developers of " + project.getName() + " are the following: " + developers.toString());
+    }
+}
